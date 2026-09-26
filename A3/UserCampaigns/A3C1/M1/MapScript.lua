09f80726403bae361bfbcc71c80d123be932e32b
pegn0ly <owafe001@gmail.com>
2026-09-26 06:55:53 +0300

    global imports logic updated

--- A3/UserCampaigns/A3C1/M1/MapScript.lua
+++ A3/UserCampaigns/A3C1/M1/MapScript.lua
@@ -2,16 +2,4 @@ while not import do
     sleep()
 end
 
-import('entry_points/adv_map_entry_point', 1)
-
-local test_data = Iterator({1, 2, 3})
-    .Map(function (item)
-        return item * 2
-    end)
-    .Collect()
-
-print("Test data: ", test_data)
-
-local creature = Creature(CREATURE_ROYAL_GRIFFIN)
-
-print("Abilities: ", creature.abilities)
+import('@entry_points/adv_map_entry_point')
--- source/autocomplete_only/core.lua
+++ source/autocomplete_only/core.lua
@@ -1,7 +1,6 @@
 --- Импортирует скрипт по заданному пути
 ---@param rel_path string относительный путь скрипта
----@param is_global? nil|1 если равно 1, то базовым путем импорта будет "scripts/lib" иначе путь к скриптам конкретной карты
-function import(rel_path, is_global)
+function import(rel_path)
     
 end
 
